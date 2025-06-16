2a1bb0c65a3c0bb47c67e7856dea4da56ca918e7
Maulana Arif Wijaya <88649781+LeToyek@users.noreply.github.com>
2025-06-16 08:40:49 +0700

    Time: 1323 ms (5.13%) | Memory: 10.3 MB (30.93%) - LeetSync

--- 49-group-anagrams/group-anagrams.go
+++ 49-group-anagrams/group-anagrams.go
@@ -1,26 +1,46 @@
-import "sort"
-
 func groupAnagrams(strs []string) [][]string {
-    groups := make(map[string][]string)
+    var allStrs [][]string
+    used := make([]bool, len(strs)) // to avoid grouping the same word multiple times
 
-    for _, str := range strs {
-        key := sortedString(str)
-        groups[key] = append(groups[key], str)
+    for i := 0; i < len(strs); i++ {
+        if used[i] {
+            continue
+        }
+
+        var tempGroup []string
+        tempGroup = append(tempGroup, strs[i])
+        used[i] = true
+
+        for j := i + 1; j < len(strs); j++ {
+            if !used[j] && isAnagram(strs[i], strs[j]) {
+                tempGroup = append(tempGroup, strs[j])
+                used[j] = true
+            }
+        }
+
+        allStrs = append(allStrs, tempGroup)
     }
 
-    result := [][]string{}
-    for _, group := range groups {
-        result = append(result, group)
-    }
-
-    return result
+    return allStrs
 }
 
-// Helper to sort a string
-func sortedString(s string) string {
-    chars := []rune(s)
-    sort.Slice(chars, func(i, j int) bool {
-        return chars[i] < chars[j]
-    })
-    return string(chars)
+func isAnagram(s string, t string) bool {
+    if len(s) != len(t) {
+        return false
+    }
+
+    count := make(map[rune]int)
+
+    for _, ch := range s {
+        count[ch]++
+    }
+
+    for _, ch := range t {
+        count[ch]--
+        if count[ch] < 0 {
+            return false
+        }
+    }
+
+    return true
 }
