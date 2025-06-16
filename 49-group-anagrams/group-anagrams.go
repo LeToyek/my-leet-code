33703515743ac923cdfb40f92bb697cdb6cfc235
Maulana Arif Wijaya <88649781+LeToyek@users.noreply.github.com>
2025-06-16 08:40:37 +0700

    Time: 8 ms (78.22%) | Memory: 9.6 MB (63.98%) - LeetSync

--- 49-group-anagrams/group-anagrams.go
+++ 49-group-anagrams/group-anagrams.go
@@ -1,46 +1,26 @@
+import "sort"
+
 func groupAnagrams(strs []string) [][]string {
-    var allStrs [][]string
-    used := make([]bool, len(strs)) // to avoid grouping the same word multiple times
+    groups := make(map[string][]string)
 
-    for i := 0; i < len(strs); i++ {
-        if used[i] {
-            continue
-        }
-
-        var tempGroup []string
-        tempGroup = append(tempGroup, strs[i])
-        used[i] = true
-
-        for j := i + 1; j < len(strs); j++ {
-            if !used[j] && isAnagram(strs[i], strs[j]) {
-                tempGroup = append(tempGroup, strs[j])
-                used[j] = true
-            }
-        }
-
-        allStrs = append(allStrs, tempGroup)
+    for _, str := range strs {
+        key := sortedString(str)
+        groups[key] = append(groups[key], str)
     }
 
-    return allStrs
+    result := [][]string{}
+    for _, group := range groups {
+        result = append(result, group)
+    }
+
+    return result
 }
 
-func isAnagram(s string, t string) bool {
-    if len(s) != len(t) {
-        return false
-    }
-
-    count := make(map[rune]int)
-
-    for _, ch := range s {
-        count[ch]++
-    }
-
-    for _, ch := range t {
-        count[ch]--
-        if count[ch] < 0 {
-            return false
-        }
-    }
-
-    return true
+// Helper to sort a string
+func sortedString(s string) string {
+    chars := []rune(s)
+    sort.Slice(chars, func(i, j int) bool {
+        return chars[i] < chars[j]
+    })
+    return string(chars)
 }
